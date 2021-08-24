e403bbf90abeb4c5af4fbf404d7e427109f12290
Tanishk <tanishkistonk@gmail.com>
2021-08-25 00:15:20 +0530

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as bodyPix from '@tensorflow-models/body-pix';
+import Webcam from 'react-webcam';
 import './App.css';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runBodySegment = async () =>
+  {
+    const net = await bodyPix.load();
+    console.log("Bodypix model loaded!");
+    setInterval(()=>{
+      detect(net);
+    },500);
+  };
+
+  const detect = async(net) =>
+  {
+    if(webcamRef.current && webcamRef.current.video.readyState === 4)
+    {
+      const video = webcamRef.current.video;
+      const videoHeight = video.videoHeight;
+      const videoWidth = video.videoWidth;
+
+      canvasRef.current.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+
+      const person = await net.segmentPersonParts(video);
+      console.log(person);
+
+      const coloredPartImage = bodyPix.toColoredPartMask(person);
+
+      bodyPix.drawMask(
+        canvasRef.current,
+        video,
+        coloredPartImage,
+        0.5,0,false
+      );
+    }
+  }
+  runBodySegment();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webcamRef}
+        style = {{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex:9,
+          width: 640,
+          height: 400,
+        }} />
+
+        <canvas ref = {canvasRef}
+          style = {{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex:9,
+            width: 640,
+            height: 400,
+          }} />
+
       </header>
     </div>
   );
